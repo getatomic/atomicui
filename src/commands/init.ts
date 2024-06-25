37370dcaf78653208116c0b5d6144076ce2ad22f
Parth Shah <parth@mynextdeveloper.com>
2024-06-25 17:48:24 +0530

    Fix: defaults

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -5,8 +5,6 @@ import { logger } from "@/utils/logger";
 export const init = new Command()
   .name("init")
   .description("initialize your project and install dependencies")
-  .option("-y, --yes", "skip confirmation prompt.", false)
-  .option("-d, --defaults,", "use default configuration.", false)
   .option(
     "-c, --cwd <cwd>",
     "the working directory. defaults to the current directory.",
