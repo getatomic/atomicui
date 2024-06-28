55b23e185fe7e0397ff4af36ab8946a33c43a463
Parth Shah <parth@mynextdeveloper.com>
2024-06-28 20:15:07 +0530

    Feat: detect repo

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -7,7 +7,10 @@ import { Command } from "commander";
 
 import { handleError } from "@/utils/handle-error";
 import { logger } from "@/utils/logger";
-import { getPackageManager } from "@/utils/get-package-info";
+import {
+  getPackageManager,
+  checkPackageExists,
+} from "@/utils/get-package-info";
 import { resolveConfigPaths, Config } from "@/utils/get-config";
 
 import { getThemes, getInitData } from "@/utils/api";
@@ -32,11 +35,32 @@ export const init = new Command()
         process.exit(1);
       }
 
+      const spinner = ora(`Validating project...`).start();
+      // check for package.json
+      if (!existsSync(path.resolve(cwd, "package.json"))) {
+        spinner.fail();
+        logger.error(
+          `No package.json found in ${cwd}. Please run "npm init" first.`
+        );
+        process.exit(1);
+      }
+
+      // check if either next or react exists
+      if (
+        !checkPackageExists("react", cwd) &&
+        !checkPackageExists("next", cwd)
+      ) {
+        spinner.fail();
+        logger.error(
+          `Missing required dependencies. Please install React or Next first.`
+        );
+        process.exit(1);
+      }
+
+      spinner.succeed();
+
       const projectConfig = await promptForProjectDetails(cwd);
-      logger.info(
-        "Project configuration saved to getatomic.components.json",
-        JSON.stringify(projectConfig)
-      );
+      logger.info("Project configuration saved to getatomic.components.json");
 
       await runInit(cwd, projectConfig);
     } catch (error) {
@@ -52,29 +76,36 @@ export const init = new Command()
 async function promptForProjectDetails(cwd: string) {
   const themes = await getThemes();
 
-  const options = await prompts([
+  const options = await prompts(
+    [
+      {
+        type: "select",
+        name: "theme",
+        message: `Which theme would you like to use?`,
+        choices: themes.map((theme) => ({
+          title: theme.name,
+          value: theme.id,
+        })),
+      },
+      {
+        type: "text",
+        name: "globalCSS",
+        message: `Where is your global CSS file?`,
+        initial: "app/globals.css",
+      },
+      {
+        type: "text",
+        name: "components",
+        message: `Configure the import alias for the atomic components.`,
+        initial: "@/components/getatomic",
+      },
+    ],
     {
-      type: "select",
-      name: "theme",
-      message: `Which theme would you like to use?`,
-      choices: themes.map((theme) => ({
-        title: theme.name,
-        value: theme.id,
-      })),
-    },
-    {
-      type: "text",
-      name: "globalCSS",
-      message: `Where is your global CSS file?`,
-      initial: "app/globals.css",
-    },
-    {
-      type: "text",
-      name: "components",
-      message: `Configure the import alias for the atomic components.`,
-      initial: "@/components/getatomic",
-    },
-  ]);
+      onCancel: () => {
+        throw new Error("Terminated Atomic UI setup.");
+      },
+    }
+  );
 
   const atomicComponentsConfig = {
     theme: {
